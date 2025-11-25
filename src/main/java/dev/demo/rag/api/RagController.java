ddb9d876791d41fc995b41a0e39e23095a9433ac
Thiago <83519346+master-code-java@users.noreply.github.com>
2025-11-25 11:25:55 +0000

    Open cors

--- src/main/java/dev/demo/rag/api/RagController.java
+++ src/main/java/dev/demo/rag/api/RagController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins="*", allowedHeaders="*", methods={})
 @RestController
 @RequestMapping("/api")
 public class RagController {
